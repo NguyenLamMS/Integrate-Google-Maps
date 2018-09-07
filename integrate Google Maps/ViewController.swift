6fbb75b10b89117929023efef106b04fd95e9311
Nguyen Lam <lam1997.ln@gmail.com>
2018-09-07 09:01:03 +0700

    Initial Commit

---
+++ integrate Google Maps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  integrate Google Maps
+//
+//  Created by Binh Phan on 9/7/18.
+//  Copyright © 2018 Binh Phan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
